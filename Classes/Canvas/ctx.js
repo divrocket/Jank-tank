b47459ec2d1d462ecc1c8c1919c3411ccb4c4908
Robert West <robert.west@fresno.gov>
2023-08-21 17:16:04 -0700

    screen resolution fix

--- Classes/Canvas/ctx.js
+++ Classes/Canvas/ctx.js
@@ -1,12 +1,9 @@
 export const canvas = document.getElementById("gameCanvas");
 export const ctx = canvas.getContext("2d");
 
-//outer fade effect
-ctx.shadowColor = "#99714bb3";
-ctx.shadowBlur = 20;
-canvas.style.border = "1px solid black";
-canvas.style.backgroundColor = "#99714bb3";
+//Inner glow effect
+canvas.style.backgroundColor = "rgba(102,255,0,0.7)";
 
-
-
+ctx.canvas.width = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
 
--- Classes/Drawing/drawProfiler.js
+++ Classes/Drawing/drawProfiler.js
@@ -1,38 +1,37 @@
 import {systemProfiler} from "../Profiling/process.js";
 import {statsConfig} from "../Profiling/config.js";
+import {ctx} from "../Canvas/ctx.js";
 
 const profiler = new systemProfiler();
 
-export function drawStatsToPage() {
-	// Get the parent container
-	const container = document.getElementById('statsContainer');
+export function drawStatsToCanvas() {
+	
+	let yPos = 100;
+	let xPos = 20;
+	
+	const lineHeight = 25;
 	
 	for (let stat of statsConfig.stats) {
-		// Check if an element with the stat's ID already exists
-		let statElement = document.getElementById(stat.elementId);
-		
-		// If it doesn't exist, create a new div for this stat
-		if (!statElement) {
-			statElement = document.createElement('div');
-			statElement.id = stat.elementId;
-			
-			// Append this new div to the parent container
-			container.appendChild(statElement);
-		}
-		
-		// Now set or update the innerHTML of the statElement
+		let text = "";
 		switch (stat.type) {
 			case 'scalar':
-				statElement.innerHTML = `<strong>${stat.description}</strong> ${profiler[stat.property]}`;
+				text = `${stat.description} ${profiler[stat.property]}`;
 				break;
 			
 			case 'arrayLength':
-				statElement.innerHTML = `<strong>${stat.description}</strong> ${profiler.getObjectArrayLength(stat.arrayName)}`;
+				text = `${stat.description} ${profiler.getObjectArrayLength(stat.arrayName)}`;
 				break;
-			
-			// You can extend this for other types in future
+				
 			default:
 				break;
 		}
+		ctx.fillText(text, xPos, yPos);
+		yPos += lineHeight;
 	}
+	
+	//Screen Resolution
+	ctx.fillText(`Screen Resolution: ${ctx.canvas.width} x ${ctx.canvas.height}`, xPos, yPos);
+	
 }
+
+
